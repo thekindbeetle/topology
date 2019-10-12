108e9c844c0319ddbfd0170bb7e4f82d4b07bd8d
Vladislav Alekseev <thekindbeetle@gmail.com>
2019-10-12 19:18:16 +0300

    Scale space drawing + parallel computation.

--- topology/jacobi/scalespacemesh3d.py
+++ topology/jacobi/scalespacemesh3d.py
@@ -3,6 +3,8 @@ import morse.field_generator as gen
 import skimage.filters
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d as plot3d
+import threading
+import time
 
 
 def test():
@@ -64,9 +66,13 @@ class ScaleSpaceMesh3D:
 
     def __init__(self, data, level_num):
         self.sizeX, self.sizeY = data.shape
+
+        # Количество уровней
         self.sizeZ = level_num
         self.f = np.zeros((self.sizeX, self.sizeY, self.sizeZ))
         self.f[:, :, 0] = data
+
+        # Данные на уровне определяются свёрткой с Гауссовским ядром
         for level in range(1, level_num):
             self.f[:, :, level] = skimage.filters.gaussian(data, sigma=level)
         self.g = np.ones((self.sizeX, self.sizeY, self.sizeZ))
@@ -128,6 +134,11 @@ class ScaleSpaceMesh3D:
     def _get_link(self, x, y, z, edge_type):
         """
         Линк ребра по начальной точке и типу ребра.
+        :param x: X coordinate
+        :param y: Y coordinate
+        :param z: Z coordinate
+        :param edge_type: edge type from EDGE_TYPE list.
+        :return:
         """
         link = []
         link_size = 0
@@ -163,10 +174,10 @@ class ScaleSpaceMesh3D:
     def _get_edge_end(self, x, y, z, edge_type):
         """
         Координаты конца ребра по его типу и начальной точке.
-        :param x:
-        :param y:
-        :param z:
-        :param edge_type:
+        :param x: X coordinate
+        :param y: Y coordinate
+        :param z: Z coordinate
+        :param edge_type: edge type from EDGE_TYPE list.
         :return:
         """
         if edge_type == 'Xx':
@@ -196,16 +207,17 @@ class ScaleSpaceMesh3D:
         Если у нижнего линка один разрыв - то ребро не критическое,
         если 0 или 2 - то критическое порядка 1,
         если 3 - то порядка 2 (паттерн 010101).
-        :param x:
-        :param y:
-        :param z:
-        :param edge_type:
+        :param x: X coordinate.
+        :param y: Y coordinate.
+        :param z: Z coordinate.
+        :param edge_type: edge type from EDGE_TYPE list.
         :return:
         """
         x1, y1, z1 = self._get_edge_end(x, y, z, edge_type)  # Находим конец ребра.
 
         # Сначала найдём множитель l = \lambda, при котором h(x) = g(x) + l f(x) на концах ребра равна.
-        # (функция g может принимать одно значение, поэтому загоняем разность g в числитель)
+        # (функция g может принимать одно значение, поэтому загоняем разность g в числитель,
+        #  а функцию f мы шевелим, поэтому её отправляем в знаменатель)
         dif_g = self.g[x1, y1, z1] - self.g[x, y, z]
         dif_f = self.f[x1, y1, z1] - self.f[x, y, z]
         k = -dif_g / dif_f
@@ -214,19 +226,17 @@ class ScaleSpaceMesh3D:
         h = self.g[x, y, z] + k * self.f[x, y, z]
         link, link_size = self._get_link(x, y, z, edge_type)
 
-        # lower_link_type = 0
-        # two_power = 1
-        # for i in range(link_size):
-        #     v = link[i]
-        #     if self.g[v[0], v[1], v[2]] + k * self.f[v[0], v[1], v[2]] < h:
-        #         lower_link_type += two_power
-        #     two_power *= 2
+        # Кодируем нижний линк двоичным кодом (1 - если точка присутствует, 2 - если нет).
+        # Переводим в десятичный и смотрим тип линка по спискам LINK_4_TYPES и LINK_6_TYPES.
+        lower_link_type = 0
+        two_power = 1
+        for i in range(link_size):
+            v = link[i]
+            if self.g[v[0], v[1], v[2]] + k * self.f[v[0], v[1], v[2]] < h:
+                lower_link_type += two_power
+            two_power *= 2
 
-        lower_link = ''.join(['1' if self.g[v[0], v[1], v[2]] + k * self.f[v[0], v[1], v[2]] < h else '0'
-                             for v in link])
-
-        return self.LINK_TYPES[lower_link]
-        # return self.LINK_4_TYPES[lower_link_type] if link_size == 4 else self.LINK_6_TYPES[lower_link_type]
+        return self.LINK_4_TYPES[lower_link_type] if link_size == 4 else self.LINK_6_TYPES[lower_link_type]
 
     def calc_scale_space_set(self, start_level=3):
         """
@@ -264,32 +274,41 @@ class ScaleSpaceMesh3D:
         :param start_level: Уровень, с которого начинаем вычисления.
         :return: в поле jacobi_set записывается множество Якоби.
         """
-        import threading
 
         self.jacobi_set = dict()
         for edge_type in ScaleSpaceMesh3D.EDGE_TYPES:
             self.jacobi_set[edge_type] = []
 
         # Мы будем писать в разные массивы рёбер в разных потоках.
-        def _cmp_edge_set_straight(edge_type):
+        def _cmp_edge_set_straight(e_type):
+            """
+            Вычисление мнлжества Якоби из диагональных рёбер заданного типа.
+            :param e_type:
+            :return:
+            """
             result = []
             for i in range(1, self.sizeX - 1):
                 for j in range(1, self.sizeY - 1):
                     for k in range(start_level, self.sizeZ - 1):
-                        if self._get_edge_criticality(i, j, k, edge_type) > 0:
-                            result.append([(i, j, k), self._get_edge_end(i, j, k, edge_type)])
-            self.jacobi_set[edge_type] = result
+                        if self._get_edge_criticality(i, j, k, e_type) > 0:
+                            result.append([(i, j, k), self._get_edge_end(i, j, k, e_type)])
+            self.jacobi_set[e_type] = result
 
-        def _cmp_edge_set_diagonal(edge_type):
+        def _cmp_edge_set_diagonal(e_type):
+            """
+            Вычисление мнлжества Якоби из диагональных рёбер заданного типа.
+            :param e_type:
+            :return:
+            """
             result = []
             for i in range(1, self.sizeX - 1):
                 for j in range(1, self.sizeY - 1):
                     for k in range(start_level, self.sizeZ - 1):
                         # Диагональные рёбра выходят только из чёрных вершин.
                         if (i + j + k) % 2 == 1:
-                            if self._get_edge_criticality(i, j, k, edge_type) > 0:
-                                result.append([(i, j, k), self._get_edge_end(i, j, k, edge_type)])
-            self.jacobi_set[edge_type] = result
+                            if self._get_edge_criticality(i, j, k, e_type) > 0:
+                                result.append([(i, j, k), self._get_edge_end(i, j, k, e_type)])
+            self.jacobi_set[e_type] = result
 
         threads = []
 
@@ -342,3 +361,150 @@ class ScaleSpaceMesh3D:
         ax = fig.add_subplot(111, projection='3d')
         for edge_type in self.EDGE_TYPES:
             self._draw_edge_set(ax, self.jacobi_set[edge_type])
+
+if __name__ == '__main__':
+    import argparse
+    import networkx as nx
+    import scipy.interpolate as sip
+    # Эта штука нужна, чтобы рисовать в 3D с картинкой на фоне.
+    import visvis as vv
+    from visvis.utils.pypoints import Pointset
+    vv.use('qt5')
+
+    print("Started...")
+
+    parser = argparse.ArgumentParser(description='Scale space computation')
+    parser.add_argument('--input', metavar='i', type=str, help='input FITS file')
+    parser.add_argument('--output', metavar='o', type=str, help='output PNG file')
+    parser.add_argument('--levels', metavar='l', type=int, default=50, help='number of levels', required=False)
+    parser.add_argument('--parallel', metavar='p', type=int, default=True, help='parallel computation', required=False)
+    args = parser.parse_args()
+
+    image = gen.gen_field_from_file(args.input, conditions='plain', filetype='fits')
+    j = ScaleSpaceMesh3D(image, level_num=args.levels)
+    if args.parallel:
+        print('Parallel computation started...')
+        j.calc_scale_space_set_parallel(start_level=6)
+    else:
+        print('Computation started...')
+        j.calc_scale_space_set(start_level=6)
+
+    print("Jacobi graph constructing...")
+    edges, nodes = [], []
+    for edge_type in j.EDGE_TYPES:
+        edges += j.jacobi_set[edge_type]
+
+    for edge in edges:
+        nodes += [edge[0], edge[1]]
+    nodes = list(set(nodes))
+
+    g = nx.Graph()
+    g.add_nodes_from(nodes)
+    g.add_edges_from(edges)
+
+    print("Remove loops...")
+    while True:
+        try:
+            g.remove_edges_from(nx.find_cycle(g))
+        except nx.NetworkXNoCycle:
+            break
+    print("Remove isolated vertices...")
+    g.remove_nodes_from([v for v in nodes if g.degree[v] == 0])
+
+    print("Recompile into paths...")
+    paths = list(nx.connected_components(g))
+    print("Number of paths: {0}".format(len(paths)))
+
+    def reduce_path_hops(p):
+        # Удаляем из пути зигзаги (см. стр. 5 части Scale-Space диссера)
+        i = 0
+        while i < len(p) - 2:
+            if p[i][2] == p[i + 2][2]:  # Эта вершина и вершина через одну лежат на одном уровне
+                del p[i + 1]  # Удаляем промежуточную точку
+            else:
+                i += 1
+    straight_paths = []
+    for p in paths:
+        p_endpoints = [node for node in p if g.degree[node] == 1]
+        p_straight = nx.shortest_path(g, p_endpoints[0], p_endpoints[1])
+        reduce_path_hops(p_straight)
+        straight_paths.append(p_straight)
+
+    print("Number of straight paths: {0}".format(len(straight_paths)))
+    print("Redirecting paths...")
+    for i in range(len(straight_paths)):
+        if straight_paths[i][0][2] > straight_paths[i][-1][2]:
+            straight_paths[i].reverse()
+
+    sizeX, sizeY = image.shape[:2]
+
+    def is_path_border(path):
+        top_point = path[-1]
+        return (top_point[0] == 0) or (top_point[0] == 0) or (top_point[0] == sizeX - 1) or (
+                    top_point[0] == sizeX - 2) or \
+               (top_point[1] == 0) or (top_point[1] == 1) or (top_point[1] == sizeY - 1) or (top_point[1] == sizeY - 2)
+
+    print("Remove paths leading into boundary")
+    normal_paths = [p for p in straight_paths if not is_path_border(p)]
+
+    print("Number of correct paths: {0}".format(len(normal_paths)))
+    long_normal_paths = [n for n in normal_paths if len(n) > 3]
+    print("Number of long paths: {0}".format(len(long_normal_paths)))
+
+    def path_top_point(path):
+        """
+        Ищем топ-точку пути.
+        Находим все точки с максимальной координатой Z, берём среднее.
+        """
+        p = np.array(path, dtype=np.int)
+        idx = np.where(p[:, 2] == p[:, 2].max())[0].mean(dtype=np.int)
+        return path[idx]
+
+    def draw_smoothed_path(ax, p, radius=1, color=(1, 0, 0), draw_source=False):
+        if draw_source:
+            x, y, z = np.transpose(p)
+            vv.plot(x, y, z, '--r', axes=ax)
+        tck, u = sip.splprep(np.transpose(p), k=3)
+        new_p = sip.splev(np.linspace(0, 1, 200), tck)
+
+        pp = Pointset(3)
+        for pt in np.transpose(new_p):
+            pp.append((pt[0], pt[1], pt[2]))
+
+        #     line = vv.solidLine(pp, radius, 8, axes=ax)
+        #     line = vv.line(pp, radius, 8, axes=ax)
+        vv.plot(pp, lw=3, lc=color, alpha=0.5, ls='-')
+
+    def draw_top_points(ax, paths, color=(1, 1, 0)):
+        top_pts = np.array([path_top_point(p) for p in paths])
+        pp = Pointset(top_pts)
+        vv.plot(pp, ms='.', mc=color, mw='30', ls='', mew=0, axes=ax)
+
+    def plot_bg_image(imgdata, level):
+        nr, nc = imgdata.shape[:2]
+        x, y = np.mgrid[:nr, :nc]
+        z = np.ones((nr, nc))
+        vv.functions.surf(x, y, z * level, imgdata, aa=3)
+
+    # Рисуем картинки
+    z_multiplier = 4  # Увеличиваем масштаб по Z
+    plot_bg_image(image, level=6)
+    ax = vv.gca()
+    ax.bgcolor = (0.7, 0.7, 0.7)
+    ax.light0.position = (200, 200, 200, 0)
+    for path in long_normal_paths:
+        if len(path) <= 3:
+            # Для кубического сплайна нужно 4 точки.
+            continue
+        draw_smoothed_path(ax, path, radius=2, color=(1, 1, 1))
+    draw_top_points(ax, long_normal_paths)
+    ax.camera.azimuth = 45.
+    ax.camera.elevation = 20.
+    ax.camera.daspect = (1, 1, z_multiplier)
+    vv.gcf().relativeFontSize = 2.0
+
+    # import time
+    # print("Wait for rendering...")
+    # time.sleep(30)
+    # vv.screenshot(args.output, ob=ax)
+
